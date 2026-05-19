4a227c695bf4372473ffaa571f4b9e5b6d891992
TechnoStudy <ismettechno@gmail.com>
2026-05-19 19:25:22 +0100

    _01_LoginSteps 2

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -8,15 +8,17 @@ public class _01_LoginSteps {
 
     @Given("Navigate to WebSite")
     public void navigate_to_web_site() {
-
+         // siteyi aç
     }
+
     @When("Enter username and password and click login button")
     public void enter_username_and_password_and_click_login_button() {
-
+        // username şifre gönder tıklat
     }
+
     @Then("User should login successfully")
     public void user_should_login_successfully() {
-
+       // siteye girildiğinin kontrolünü yap
     }
 
 }

0b1e16005b3a27953a1e0632ab8da2116a77c6f7
TechnoStudy <ismettechno@gmail.com>
2026-05-19 19:23:35 +0100

    _01_LoginSteps

---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,22 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_LoginSteps {
+
+    @Given("Navigate to WebSite")
+    public void navigate_to_web_site() {
+
+    }
+    @When("Enter username and password and click login button")
+    public void enter_username_and_password_and_click_login_button() {
+
+    }
+    @Then("User should login successfully")
+    public void user_should_login_successfully() {
+
+    }
+
+}

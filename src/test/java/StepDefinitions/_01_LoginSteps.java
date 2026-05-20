c37a0be69c8c5002cc267d1337f56fd67152042b
TechnoStudy <ismettechno@gmail.com>
2026-05-20 18:39:48 +0100

    GWD 1

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import Utilities.GWD;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,18 +8,22 @@ import io.cucumber.java.en.When;
 public class _01_LoginSteps {
 
     @Given("Navigate to WebSite")
-    public void navigate_to_web_site() {
-         // siteyi aç
+    public void navigateToWebSite() {
+        //sitenin açılacağı yer
+        GWD.getDriver().get("https://opencart.abstracta.us/index.php?route=account/login");
     }
 
     @When("Enter username and password and click login button")
-    public void enter_username_and_password_and_click_login_button() {
-        // username şifre gönder tıklat
+    public void enterUsernameAndPasswordAndClickLoginButton() {
+        //username password gönder login e tıklat
+       //pomSayfa.username.SendKeys("username");
+       // pomSayfa.password.SendKeys("password");
+       // pomSayfa.LoginButton.Click();
     }
 
     @Then("User should login successfully")
-    public void user_should_login_successfully() {
-       // siteye girildiğinin kontrolünü yap
+    public void userShouldLoginSuccessfully() {
+        //girildiğini validate et
     }
 
 }
---
+++ src/test/java/Utilities/GWD.java
@@ -0,0 +1,18 @@
+package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GWD {
+
+    public static WebDriver getDriver(){
+        WebDriver driver=new ChromeDriver();
+
+
+        return driver;
+    }
+
+
+
+
+}

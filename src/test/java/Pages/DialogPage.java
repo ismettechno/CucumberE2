d12dddd2895f1570b6d8c1f53652d6b4b40466a6
TechnoStudy <ismettechno@gmail.com>
2026-05-20 19:42:23 +0100

    POM 2 mySendKeys myClick

--- src/test/java/Pages/DialogPage.java
+++ src/test/java/Pages/DialogPage.java
@@ -1,11 +1,17 @@
 package Pages;
 
 import Utilities.GWD;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class DialogPage {
+    WebDriverWait wait=new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(10));
 
     public DialogPage() {
         PageFactory.initElements(GWD.getDriver(),this);
@@ -22,17 +28,22 @@ public class DialogPage {
 
     public void mySendKeys(WebElement e, String text)
     {
-        //beklemesi
-        //scroll
-        //clear
-        //sendKeys
+        wait.until(ExpectedConditions.visibilityOf(e));
+        scrollToElement(e);
+        e.clear();
+        e.sendKeys(text);
     }
 
     public void myClick(WebElement e)
     {
-        //beklemesi
-        //scroll
-        //click
+        wait.until(ExpectedConditions.elementToBeClickable(e));
+        scrollToElement(e);
+        e.click();
+    }
+
+    public void scrollToElement(WebElement e){
+        JavascriptExecutor js=(JavascriptExecutor) GWD.getDriver();
+        js.executeScript("arguments[0].scrollIntoView();", e);
     }
 
 

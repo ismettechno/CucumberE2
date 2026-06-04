d90424c18795ff9a77bdfc93f24ef6a0a101adfd
TechnoStudy <ismettechno@gmail.com>
2026-06-04 18:24:40 +0100

    Extend Report 6 - 2

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -2,12 +2,23 @@ package StepDefinitions;
 
 import Utilities.GWD;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
     @After // after senaryo
-    public void after()
+    public void after(Scenario senaryo)
     {
+
+        if (senaryo.isFailed())
+        {
+            TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
+            byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
+            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
+        }
+
         GWD.quitDriver();
     }
 }

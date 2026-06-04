81ac5ee2dafcc222f9dc0cea5bc9032410e9e349
TechnoStudy <ismettechno@gmail.com>
2026-06-04 18:37:01 +0100

    Extend Report 8

--- src/test/java/Runners/_07_TestRunnerExtentReport.java
+++ src/test/java/Runners/_07_TestRunnerExtentReport.java
@@ -11,4 +11,7 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class _07_TestRunnerExtentReport extends AbstractTestNGCucumberTests {
+
+
+
 }

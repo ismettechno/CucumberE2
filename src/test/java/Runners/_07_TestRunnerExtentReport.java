a5644f819c10164b50fbe4918c45e23719436fb3
TechnoStudy <ismettechno@gmail.com>
2026-06-04 18:54:42 +0100

    Paralel Çalışma 1

--- src/test/java/Runners/_07_TestRunnerExtentReport.java
+++ src/test/java/Runners/_07_TestRunnerExtentReport.java
@@ -1,8 +1,10 @@
 package Runners;
 
 
+import com.aventstack.extentreports.service.ExtentService;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
 @CucumberOptions(
         tags = "@SmokeTest",
@@ -12,6 +14,20 @@ import io.cucumber.testng.CucumberOptions;
 )
 public class _07_TestRunnerExtentReport extends AbstractTestNGCucumberTests {
 
+    @AfterClass
+    public  static void writeExtendReport()
+    {
+        ExtentService.getInstance().setSystemInfo("Windows User Name", System.getProperty("user.name")+" -> ELA");
+        ExtentService.getInstance().setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("User Name", "İsmet Temur");
+        ExtentService.getInstance().setSystemInfo("Application Name", "Opencart");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department", "QA");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
 
+    }
 
 }

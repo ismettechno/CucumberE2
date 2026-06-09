e38448bfbce9434725b95795a5488db268d09c0e
TechnoStudy <ismettechno@gmail.com>
2026-06-09 19:09:25 +0100

    _04_ApachePOIGetAllData

--- src/test/java/ApahePOI/_03_ApachePOIStart.java
+++ src/test/java/ApahePOI/_03_ApachePOIStart.java
@@ -9,22 +9,22 @@ import java.io.IOException;
 public class _03_ApachePOIStart {
 
     public static void main(String[] args) throws IOException {
-        String path="src/test/java/ApahePOI/Resource/ApacheExcel2.xlsx";
+        String path = "src/test/java/ApahePOI/Resource/ApacheExcel2.xlsx";
 
-     // Dosya yı aç yani bağlantı kur
-        FileInputStream dosyaOkumaBaglantisi=new FileInputStream(path);
+        // Dosya yı aç yani bağlantı kur
+        FileInputStream dosyaOkumaBaglantisi = new FileInputStream(path);
 
-     // Dosyadaki WorkBook u oku ve Hafızaya yükle
-        Workbook calismaKitabi= WorkbookFactory.create(dosyaOkumaBaglantisi);
+        // Dosyadaki WorkBook u oku ve Hafızaya yükle
+        Workbook calismaKitabi = WorkbookFactory.create(dosyaOkumaBaglantisi);
 
-     // Çalışma sayfasını seç
-       Sheet calismaSayfasi= calismaKitabi.getSheetAt(0); // getSheet("isim")
+        // Çalışma sayfasını seç
+        Sheet calismaSayfasi = calismaKitabi.getSheetAt(0); // getSheet("isim")
 
-     // Satırı seç
-        Row satir= calismaSayfasi.getRow(0);  // 0.Satır
+        // Satırı seç
+        Row satir = calismaSayfasi.getRow(0);  // 0.Satır
 
-     // hücreyi oku
-        Cell hucre=satir.getCell(0);
+        // hücreyi oku
+        Cell hucre = satir.getCell(0);
 
         System.out.println("hucre = " + hucre);
     }

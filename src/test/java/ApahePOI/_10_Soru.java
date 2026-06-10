8835fe2ab115778939b125f4be875b20a93b56a3
TechnoStudy <ismettechno@gmail.com>
2026-06-10 19:18:09 +0100

    _10_Soru:  Soru

---
+++ src/test/java/ApahePOI/_10_Soru.java
@@ -0,0 +1,22 @@
+package ApahePOI;
+
+
+// YeniExcel2.xlsx adında bir yeni excele merhaba Dünya 0 şeklinde yazı yazınız.
+// Daha sonra program her yeniden çalıştırıldığında altına Merhaba Dünya 1.2.3
+// şeklinde eklesin. Yani dosya yok ise oluşturup yazsın, daha sonra hep altına eklesin.
+
+// File dosya=new File(path)
+// if ( dosya.exists() )    dosya var ise
+
+// Merhaba Dünya 1
+// Merhaba Dünya 2
+// Merhaba Dünya 3
+// ...
+public class _10_Soru {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}

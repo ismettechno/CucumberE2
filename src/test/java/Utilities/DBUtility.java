ccf06848c1674c4e702e9a0548bf8c00a2bfb184
TechnoStudy <ismettechno@gmail.com>
2026-06-22 19:44:52 +0100

    JDBC giriş

---
+++ src/test/java/Utilities/DBUtility.java
@@ -0,0 +1,9 @@
+package Utilities;
+
+public class DBUtility {
+
+    // ham bilgilerin tutulduğu yerden benim verdiğim kritere göre billeri al
+    // 2 boyutlu bir list olarak bana ver
+    // getDataFromDB metodu oluşturmak üzere
+
+}

783dc9e91d41b717e9cb814b4206e8d569509f88
TechnoStudy <ismettechno@gmail.com>
2026-07-06 19:47:03 +0100

    Jenkins için gerekli değişiklikler yapıldı: regression test e plugin json eklendi

--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -7,6 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@Regression",  //"@SmokeTest or @Regression",
         features = {"src/test/java/FeatureFiles"},// hepsi demek
         glue = {"StepDefinitions"}
+        ,plugin= {"json:target/cucumber/cucumber.json"} // Jenkins için JSON report
 )
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
 }
